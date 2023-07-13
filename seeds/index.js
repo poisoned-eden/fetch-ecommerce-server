74140e5ee5098e8e32b7a819016f056a4d3e6f18
poisoned-eden <poisoned.eden@gmail.com>
2023-07-13 14:07:05 +0100

    corrected references in models for foreign keys

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,15 @@
+const sequelize = require('../config/connection');
+
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
 
 const seedAll = async () => {
 	await sequelize.sync({ force: true });
 	console.log('\n----- DATABASE SYNCED -----\n');
+
 	await seedCategories();
 	console.log('\n----- CATEGORIES SEEDED -----\n');
 
